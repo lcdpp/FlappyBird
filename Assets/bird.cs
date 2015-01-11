31659df2e3f18e82c5af81f191a7eafcd16e1ac4
lcdpp <lcd202@qq.com>
2015-01-12 00:22:11 +0800

    create one scene and a bird

---
+++ Assets/bird.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bird : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		this.rigidbody2D.velocity = new Vector3(5, 0, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0))
+		{
+			Vector3 val = this.rigidbody2D.velocity;
+			this.rigidbody2D.velocity = new Vector3(val.x, 5, val.z);
+		}
+	}
+}
---
+++ Assets/camera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera : MonoBehaviour {
+
+	private GameObject bird;
+	private Transform birdTransform;
+
+	// Use this for initialization
+	void Start () {
+		bird = GameObject.FindGameObjectWithTag("Player");
+		birdTransform = bird.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 birdPos = birdTransform.position;
+		Vector3 cameraPos = this.transform.position;
+		this.transform.position = new Vector3(birdPos.x, birdPos.y, cameraPos.z);
+	}
+}
